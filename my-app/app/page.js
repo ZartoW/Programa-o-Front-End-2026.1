45db76b31f26400a4fb8c2f69615cb861459f8ba
ZartoW <jr.souza.2006@gmail.com>
2026-02-12 14:10:21 -0300

    Atualizando layout com container de carnaval

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -9,11 +9,19 @@ export default function Home() {
       justifyContent: "center",
       alignItems: "center",
       background: "linear-gradient(45deg, purple, yellow, green)",
-      color: "white",
       textAlign: "center"
     }}>
-      <h1> Feliz Carnaval, minha Professor! </h1>
+      <div style={{
+        background: "black",
+        padding: "40px",
+        borderRadius: "20px",
+        boxShadow: "0 10px 30px rgba(0,0,0,0.3)",
+        textAlign: "center",
+        width: "400px"
+      }}>
+      <h1> Feliz Carnaval, meu Professor! </h1>
       <p>Que a alegria contagie todo mundo da sua familia e que curta esse periodo de folia</p>
+      </div>
     </main>
   );
 }
